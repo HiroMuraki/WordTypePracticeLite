90f57ebe4b5058fedee2583968bab2c8b2a5bd43
Hiro Muraki <56928793+HiroMuraki@users.noreply.github.com>
2020-07-06 00:45:48 +0800

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Resources;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using WordTypePracticeLite;
